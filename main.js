b3ac1fcf5ba95a42296c8337b96381e3ba5ff39f
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-07-24 21:53:48 -0500

    feat: basic carousel effect for welcome images

--- main.js
+++ main.js
@@ -2,13 +2,25 @@
 let serviceButtons = document.querySelectorAll('.serviceName');
 serviceButtons.forEach(button => button.addEventListener('click', redirectServices))
 
+//revert back to original button color
 function redirectServices(event){
     event.target.classList.add('clickedServiceButton')
     setTimeout(() => {
         event.target.classList.remove('clickedServiceButton');
     }, 1000)
-
-    // route to the appropriate service section
 }
 
 // carousel effect for welcome images 
+const rotatingImages = [
+    '/assets/nailsBehindHead.jpg',
+    '/assets/blueDressNails.jpg'
+];
+
+let index = 0;
+const carouselImage = document.getElementById('carouselImage');
+
+setInterval(() => {
+
+    index = (index + 1) % rotatingImages.length;
+    carouselImage.src = rotatingImages[index];
+}, 3000);
